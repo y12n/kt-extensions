dbfb2321c0510f9a64bc89ce2da42e9f7c83892f
Yanagimoto Shin <s@yanagimoto.org>
2019-09-04 16:13:59 +0900

    minor fix.

--- src/main/kotlin/org/yanagimoto/extensions/String.kt
+++ src/main/kotlin/org/yanagimoto/extensions/String.kt
@@ -1,7 +1,8 @@
 package org.yanagimoto.extensions
 
+import java.net.URLDecoder
 import java.net.URLEncoder
 
-fun String.urlEncode(encoding: String = "UTF-8"): String {
-  return URLEncoder.encode(this, encoding)
-}
+fun String.urlEncode(encoding: String = "UTF-8") = URLEncoder.encode(this, encoding)
+
+fun String.urlDecode(encoding: String = "UTF-8") = URLDecoder.decode(this, encoding)
